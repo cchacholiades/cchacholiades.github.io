973b3ed25c1212b36bee5112d72d5f6a03451e8b
Chris Chacholiades <chris.chacholiades@gmail.com>
2018-11-10 19:23:34 +0000

    background effect on scroll

--- js/site.js
+++ js/site.js
@@ -244,19 +244,23 @@ $(document).ready(function() {
  // execute above function
 
  initPhotoSwipeFromDOM('.photoswipe-wrapper');
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function() {scrollFunction()};
 
-// function scrollFunction() {
-//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//         document.getElementById("myBtn").style.display = "block";
-//     } else {
-//         document.getElementById("myBtn").style.display = "none";
-//     }
-// }
+$('body').scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    $('.bg').addClass('blur-bg');
+});
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+$(document).ready(function()
+{
+    $("body").scroll(function()
+    {
+        var div = $(this);
+        if (div[0].scrollHeight - div.scrollTop() == div.height())
+        {
+        }
+        else if(div.scrollTop() == 0)
+        {
+            $('.bg').removeClass('blur-bg');
+        }
+    });
+});
